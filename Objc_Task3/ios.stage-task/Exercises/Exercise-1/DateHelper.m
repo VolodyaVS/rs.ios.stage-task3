68648a44b76a4c66648148b0c15a8186d7f5616f
Best Coder <8723499-best.coder@users.noreply.gitlab.com>
2021-05-24 17:39:00 +0300

    docs: update my solution

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,49 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+    if (monthNumber >= 1 && monthNumber <= 12) {
+        NSDateFormatter *formatter = [NSDateFormatter new];
+        NSArray *monthNames = [formatter standaloneMonthSymbols];
+        NSString *monthName = [monthNames objectAtIndex:(monthNumber - 1)];
+        return monthName;
+    }
     return nil;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    
+    NSDate *dateNew = [formatter dateFromString:date];
+    
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay fromDate:dateNew];
+    NSInteger day = [components day];
+    
+    return day;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"RU"];
+    [formatter setDateFormat:@"EE"];
+    NSString *weekDay = [formatter stringFromDate:date];
+    
+    return weekDay;
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSDate *startDate = nil;
+    NSTimeInterval interval = 0.0;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    [calendar rangeOfUnit:NSCalendarUnitWeekOfYear startDate: &startDate interval: &interval forDate: [NSDate date]];
+    NSDate *endDate = [calendar dateByAddingUnit:NSCalendarUnitSecond value: (NSInteger)interval toDate: startDate options:NSCalendarMatchNextTime];
+    return [date compare:startDate] == NSOrderedDescending && [date compare:endDate] == NSOrderedAscending;
 }
 
 @end
